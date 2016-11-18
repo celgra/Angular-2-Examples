405870f1500a24f176da392aa145accef4e1ce28
piq9117 <piq9117@gmail.com>
2016-11-18 16:16:40 -0600

    ngrx store

--- src/app/tony.component.ts
+++ src/app/tony.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import {TonyService} from './tony.service';
+import { TonyService } from './tony.service';
 
 @Component({
-    selector:"tony",
-    templateUrl: "tony.component.html"
+    selector: 'tony',
+    templateUrl: 'tony.component.html'
 })
 
-export class TonyComponent {
-
-    constructor(private tonyService: TonyService) {
-
-
-    }
+export class TonyComponent implements OnInit {
+  constructor(private tonyService: TonyService) {}
+  ngOnInit() {}
 }
