b760c2f4228817a927dd82a25e502ffc66e4a875
Celedonio Gracia <celedonio@DATALOGIC.local>
2016-11-18 16:09:06 -0600

    Latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroListComponent } from './hero-list.component';
 import { SimpleCounterComponent } from './simple-counter.component';
 import { ServiceCounterComponent } from './service-counter.component';
+import { TonyComponent } from './tony.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/hero-list', pathMatch: 'full' },
     { path: 'hero-list', component: HeroListComponent},
     { path: 'simple-counter', component: SimpleCounterComponent},
-    { path: 'service-counter', component: ServiceCounterComponent}
+    { path: 'service-counter', component: ServiceCounterComponent},
+    { path: 'tony', component: TonyComponent}
 ];
 
 @NgModule({
